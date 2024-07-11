f9acef633be3599a59cbc705be86df4ab010ebd5
Gavin Panella <gavinpanella@gmail.com>
2024-07-12 00:58:32 +0200

    Try to make `Chain` faster by making it a `Vec<Vec<u8>>`

--- src/chain.rs
+++ src/chain.rs
@@ -1,76 +1,57 @@
 #[derive(Default)]
-pub enum Chain {
-    Link(Vec<u8>, Box<Chain>),
-    #[default]
-    End,
-}
+pub struct Chain(Vec<Vec<u8>>);
 
-const CAP_MIN: usize = 2usize.pow(22);
+const CAP_MIN: usize = 2usize.pow(20);
 
 impl Chain {
     pub fn extend(mut self, bytes: &[u8]) -> Self {
-        match self {
-            Self::Link(ref mut buffer, _) if buffer.capacity() >= buffer.len() + bytes.len() => {
+        match self.0.last_mut() {
+            Some(buffer) if buffer.capacity() >= buffer.len() + bytes.len() => {
                 buffer.extend(bytes);
                 self
             }
-            Self::Link(_, _) | Self::End => {
+            Some(_) | None => {
                 let capacity = bytes.len().min(CAP_MIN);
                 let mut buffer = Vec::<u8>::with_capacity(capacity);
                 buffer.extend(bytes);
-                Self::Link(buffer, self.into())
+                self.0.push(buffer);
+                self
             }
         }
     }
 
     pub fn push(mut self, byte: u8) -> Self {
-        match self {
-            Self::Link(ref mut buffer, _) if buffer.capacity() > buffer.len() => {
+        match self.0.last_mut() {
+            Some(buffer) if buffer.capacity() > buffer.len() => {
                 buffer.push(byte);
                 self
             }
-            Self::Link(_, _) | Self::End => {
+            Some(_) | None => {
                 let mut buffer = Vec::<u8>::with_capacity(CAP_MIN);
                 buffer.push(byte);
-                Self::Link(buffer, self.into())
+                self.0.push(buffer);
+                self
             }
         }
     }
 
     pub fn _capacity(&self) -> usize {
-        match self {
-            Self::Link(buffer, prev) => buffer.capacity() + prev._capacity(),
-            Self::End => 0,
-        }
+        self.0.iter().map(Vec::capacity).sum()
     }
 
-    pub fn _len(&self) -> usize {
-        match self {
-            Self::Link(buffer, prev) => buffer.len() + prev._len(),
-            Self::End => 0,
-        }
+    pub fn len(&self) -> usize {
+        self.0.iter().map(Vec::len).sum()
     }
 
     pub fn write_to(&self, out: &mut Vec<u8>) {
-        if let Self::Link(buffer, prev) = self {
-            prev.write_to(out);
+        for buffer in self.0.iter() {
             out.extend(buffer);
         }
     }
 
     pub fn to_vec(&self) -> Vec<u8> {
-        self.to_vec_help(0)
-    }
-
-    fn to_vec_help(&self, len: usize) -> Vec<u8> {
-        match self {
-            Self::Link(buffer, prev) => {
-                let len = len + buffer.len();
-                let mut out = prev.to_vec_help(len);
-                out.extend(buffer);
-                out
-            }
-            Self::End => Vec::<u8>::with_capacity(len),
-        }
+        let mut out = Vec::with_capacity(self.len());
+        self.write_to(&mut out);
+        out
     }
 }
